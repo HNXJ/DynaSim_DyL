60f2e3af1b72f30f14e821de547a143266abe8ab
Hamed Nejat <hamednejat7@gmail.com>
2021-11-05 01:59:09 +0330

    V1.0.0

--- main.m
+++ main.m
@@ -13,57 +13,34 @@ eqns={
   'monitor iGABAa.functions, iAMPA.functions'
 };
 
+model_size = 5;
 s=[];
-s.populations(1).name='E';
-s.populations(1).size=10;
-s.populations(1).equations=eqns;
-s.populations(1).mechanism_list={'iNa','iK'};
-s.populations(1).parameters={'Iapp',9,'gNa',120,'gK',36,'noise',10};
 
-s.populations(2).name='I';
-s.populations(2).size=10;
-s.populations(2).equations=eqns;
-s.populations(2).mechanism_list={'iNa','iK'};
-s.populations(2).parameters={'Iapp',15,'gNa',120,'gK',36,'noise',20};
+for i = 1:model_size
 
-s.populations(3).name='Q';
-s.populations(3).size=10;
-s.populations(3).equations=eqns;
-s.populations(3).mechanism_list={'iNa','iK'};
-s.populations(3).parameters={'Iapp',19,'gNa',120,'gK',36,'noise',50};
+    s.populations(i).name= ['P', num2str(i)];
+    s.populations(i).size=10;
+    s.populations(i).equations=eqns;
+    s.populations(i).mechanism_list={'iNa','iK'};
+    s.populations(i).parameters={'Iapp',9,'gNa',120,'gK',36,'noise',10};
 
-disp("Populations created ...");
+end
 
-s.connections(1).direction='I->E';
-s.connections(1).mechanism_list={'iGABAa'};
-s.connections(1).parameters={'tauD',3,'gGABAa',.1,'netcon',ones(10,10)}; % connectivity matrix defined using a string that evalutes to a numeric matrix
+disp("Populations created...");
 
-s.connections(2).direction='E->I';
-s.connections(2).mechanism_list={'iAMPA'};
-s.connections(2).parameters={'tauD',3,'gAMPA',.1,'netcon',ones(10,10)}; % connectivity set using a numeric matrix defined in script
+for i = 1:model_size
+    for j = 1:model_size
 
-s.connections(3).direction='Q->I';
-s.connections(3).mechanism_list={'iAMPA'};
-s.connections(3).parameters={'tauD',3,'gAMPA',.1,'netcon',ones(10,10)}; % connectivity set using a numeric matrix defined in script
+        s.connections((i-1)*model_size + j).direction = [s.populations(i).name, '->', s.populations(j).name];
+        s.connections((i-1)*model_size + j).mechanism_list={'iGABAa'};
+        s.connections((i-1)*model_size + j).parameters={'tauD', 3, 'gGABAa', .1, 'netcon', ones(10,10)}; 
+        
+    end
+end
 
-s.connections(4).direction='E->Q';
-s.connections(4).mechanism_list={'iAMPA'};
-s.connections(4).parameters={'tauD',3,'gAMPA',.1,'netcon',ones(10,10)}; % connectivity set using a numeric matrix defined in script
-
-s.connections(5).direction='Q->E';
-s.connections(5).mechanism_list={'iAMPA'};
-s.connections(5).parameters={'tauD',3,'gAMPA',.1,'netcon',ones(10,10)}; % connectivity set using a numeric matrix defined in script
-
-s.connections(6).direction='I->Q';
-s.connections(6).mechanism_list={'iAMPA'};
-s.connections(6).parameters={'tauD',3,'gAMPA',.1,'netcon',ones(10,10)}; % connectivity set using a numeric matrix defined in script
-
-disp("Connections created.");
-
-% Simulate Sparse Pyramidal-Interneuron-Network-Gamma (sPING)
+disp("Connections created...");
+%%
 data=dsSimulate(s);
+dsPlot(data);
 
-dsPlot(data); % <-- Figure 4 in DynaSim paper
-
-
-
+%%
