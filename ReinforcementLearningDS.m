07387557706196970d514879a7eec6eb28f930b8
Hamed Nejat <hamednejat7@gmail.com>
2021-11-05 07:24:19 +0330

    V1.0.1

--- ReinforcementLearningDS.m
+++ ReinforcementLearningDS.m
@@ -1,6 +1,6 @@
 %%
 
-clc;
+clear;clc;
 
 eqns={
   'dv/dt=Iapp+@current+noise*randn(1,N_pop); Iapp=0.1; noise=1'
@@ -15,7 +15,19 @@ s.populations(1).equations = eqns;
 s.populations(1).mechanism_list = {'iNa', 'iK'};
 s.populations(1).parameters = {'Iapp', 9, 'gNa', 120, 'gK', 40, 'noise', 5};
 
-s.connections(1).direction = [s.populations(1).name, '->', s.populations(1).name];
+s.populations(1).name = 'P2';
+s.populations(1).size = 5;
+s.populations(1).equations = eqns;
+s.populations(1).mechanism_list = {'iNa', 'iK'};
+s.populations(1).parameters = {'Iapp', 9, 'gNa', 120, 'gK', 40, 'noise', 5};
+
+s.populations(1).name = 'P3';
+s.populations(1).size = 5;
+s.populations(1).equations = eqns;
+s.populations(1).mechanism_list = {'iNa', 'iK'};
+s.populations(1).parameters = {'Iapp', 9, 'gNa', 120, 'gK', 40, 'noise', 5};
+
+s.connections(1).direction = [s.populations(1).name, '->', s.populations(2).name];
 s.connections(1).mechanism_list={'iGABAa'};
 s.connections(1).parameters={'tauD', 3, 'gGABAa', .1, 'netcon', ones(5, 5)}; 
 
