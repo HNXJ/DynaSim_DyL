9fa2d35840085027c45883c43d3549a6666f59e7
Hamed Nejat <hamednejat7@gmail.com>
2021-11-08 15:29:10 +0330

    V1.0.4

--- ReinforcementLearningDS.m
+++ ReinforcementLearningDS.m
@@ -12,6 +12,7 @@
 % Extract model properties from data, e.g. tSNE and similarities between frames
 
 % NeuroPy Idea: tSNE on single trial, 100ms tPSDs as 45 data. Perplx ~= 5
+% Augmentation via adding another similar trial, e.g. trials 61 and 62 which both
 
 clear;clc;
 
