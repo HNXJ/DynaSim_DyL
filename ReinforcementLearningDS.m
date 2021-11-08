f764c74d534271f4ae62fde4c1a6aa78b2f99571
Hamed Nejat <hamednejat7@gmail.com>
2021-11-08 15:15:43 +0330

    V1.0.4

--- ReinforcementLearningDS.m
+++ ReinforcementLearningDS.m
@@ -7,7 +7,7 @@
 % Two population one-hot [0, 1] and its 4 states may be good form of output for the neural model''s net
 % Should find a way for representing plasticity (learning/memory variables)
 % Relation between frequency and gNa/gK?
-% Look for Brian2 package of Python, 
+% Look for Brian2 package of Python, Izhikevich_2007 as RL model
 
 clear;clc;
 
