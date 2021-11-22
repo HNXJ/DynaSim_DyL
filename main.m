e99b46ad427546dec569294a9d0cfc0f6b5b2d00
Hamed Nejat <hamednejat7@gmail.com>
2021-11-22 16:23:10 +0330

    V1.1.0

--- main.m
+++ main.m
@@ -1,29 +1,57 @@
-% define equations of cell model (same for E and I populations)
-eqns={
-  'dv/dt=Iapp+@current+noise*randn(1,N_pop); Iapp=0.1; noise=0'
+% Equations
+
+eqns1={
+  'dv/dt=Iapp+@current+noise*randn(1,N_pop); Iapp=0.1; noise=0.1'
   'monitor iGABAa.functions, iAMPA.functions'
 };
-% Tip: monitor all functions of a mechanism using: monitor MECHANISM.functions
 
-% create DynaSim specification structure
+eqns2={
+  'dv/dt=Iapp+@current+noise*randn(1,N_pop); Iapp=0.1; noise=0.1'
+  'monitor iGABAa.functions, iAMPA.functions'
+};
+
+eqns = eqns1;
 s=[];
-s.populations(1).name='E';
+
+s.populations(1).name='L1';
 s.populations(1).size=10;
 s.populations(1).equations=eqns;
 s.populations(1).mechanism_list={'iNa','iK'};
-s.populations(1).parameters={'Iapp',5,'gNa',120,'gK',36,'noise',40};
-s.populations(2).name='I';
+s.populations(1).parameters={'Iapp',0,'gNa',120,'gK',36,'noise',40};
+
+s.populations(2).name='L2';
 s.populations(2).size=10;
 s.populations(2).equations=eqns;
 s.populations(2).mechanism_list={'iNa','iK'};
 s.populations(2).parameters={'Iapp',0,'gNa',120,'gK',36,'noise',10};
-s.connections(1).direction='I->E';
-s.connections(1).mechanism_list={'iGABAa'};
-s.connections(1).parameters={'tauD',10,'gGABAa',.1,'netcon','ones(N_pre,N_post)'}; % connectivity matrix defined using a string that evalutes to a numeric matrix
-s.connections(2).direction='E->I';
-s.connections(2).mechanism_list={'iAMPA'};
-s.connections(2).parameters={'tauD',2,'gAMPA',.1,'netcon',ones(10,10)}; % connectivity set using a numeric matrix defined in script
-% Simulate Sparse Pyramidal-Interneuron-Network-Gamma (sPING)
-data=dsSimulate(s);
 
-dsPlot(data); % <-- Figure 4 in DynaSim paper
+s.populations(3).name='L3';
+s.populations(3).size=10;
+s.populations(3).equations=eqns;
+s.populations(3).mechanism_list={'iNa','iK'};
+s.populations(3).parameters={'Iapp',0,'gNa',120,'gK',36,'noise',10};
+
+s.populations(4).name='L4';
+s.populations(4).size=10;
+s.populations(4).equations=eqns;
+s.populations(4).mechanism_list={'iNa','iK'};
+s.populations(4).parameters={'Iapp',0,'gNa',120,'gK',36,'noise',10};
+
+s.populations(5).name='L5';
+s.populations(5).size=10;
+s.populations(5).equations=eqns;
+s.populations(5).mechanism_list={'iNa','iK'};
+s.populations(5).parameters={'Iapp',0,'gNa',120,'gK',36,'noise',10};
+
+s.connections(1).direction='L1->L2';
+s.connections(1).mechanism_list={'iGABAa'};
+s.connections(1).parameters={'tauD',10,'gGABAa',.1,'netcon','ones(N_pre,N_post)'};
+
+s.connections(2).direction='L5->L2';
+s.connections(2).mechanism_list={'iAMPA'};
+s.connections(2).parameters={'tauD',2,'gAMPA',.1,'netcon', 'ones(N_pre,N_post)'}; 
+
+data=dsSimulate(s);
+dsPlot(data); 
+
+% <-- Figure 4 in DynaSim paper
