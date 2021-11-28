ecd564a07a5487764d09aed4ca7e2d53f2f0fed5
Hamed Nejat <hamednejat7@gmail.com>
2021-11-28 16:30:11 +0330

    V1.1.2

--- main.m
+++ main.m
@@ -1,12 +1,13 @@
 % Equations
 
 eqns1={
-  'dv/dt=Iapp+0.8*@current+noise*randn(1,N_pop); Iapp=0.1; noise=0.1'
+  'u(t) = (t < 50)'
+  'dv/dt=Iapp*u(t)+0.93*@current+noise*randn(1,N_pop); noise=0.1'
   'monitor iGABAa.functions, iAMPA.functions'
 };
 
 eqns2={
-  'dv/dt=Iapp+0.4*@current+noise*randn(1,N_pop); Iapp=0.1; noise=0.1'
+  'dv/dt=Iapp+0.19*@current+noise*randn(1,N_pop); Iapp=0.1; noise=0.1'
   'monitor iGABAa.functions, iAMPA.functions'
 };
 
@@ -24,19 +25,19 @@ s.populations(2).name='L2';
 s.populations(2).size=8;
 s.populations(2).equations=eqns1;
 s.populations(2).mechanism_list={'iNa','iK'};
-s.populations(2).parameters={'Iapp',5,'gNa',90,'gK',12,'noise',25};
+s.populations(2).parameters={'Iapp',4,'gNa',90,'gK',12,'noise',25};
 
 s.populations(3).name='L3';
 s.populations(3).size=5;
 s.populations(3).equations=eqns1;
 s.populations(3).mechanism_list={'iNa','iK'};
-s.populations(3).parameters={'Iapp',3,'gNa',70,'gK',48,'noise',25};
+s.populations(3).parameters={'Iapp',3,'gNa',70,'gK',24,'noise',25};
 
 s.populations(4).name='L4';
 s.populations(4).size=12;
 s.populations(4).equations=eqns2;
 s.populations(4).mechanism_list={'iNa','iK'};
-s.populations(4).parameters={'Iapp',9,'gNa',360,'gK',24,'noise',10};
+s.populations(4).parameters={'Iapp',9,'gNa',360,'gK',48,'noise',10};
 
 s.populations(5).name='L5';
 s.populations(5).size=6;
