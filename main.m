a2dfe62441a7ea4f71bcd1743b212deda8b8229e
Hamed Nejat <hamednejat7@gmail.com>
2021-11-28 16:44:48 +0330

    V1.1.2

--- main.m
+++ main.m
@@ -3,13 +3,13 @@
 clc;
 eqns1={
 %   'u(t) = (t < 50)'
-  'dv/dt=Iapp*0.01+0.93*@current+noise*randn(1,N_pop); noise=0.1'
+  'dv/dt=Iapp*0.01+0.93*@current+noise*randn(1,N_pop);'
   'monitor iGABAa.functions, iAMPA.functions'
 };
 
 eqns2={
   'u(t) = (t < 50)'
-  'dv/dt=Iapp*u(t)+0.19*@current+noise*randn(1,N_pop); Iapp=0.1; noise=0.1'
+  'dv/dt=Iapp*u(t)+0.19*@current+noise*randn(1,N_pop);'
   'monitor iGABAa.functions, iAMPA.functions'
 };
 
@@ -21,31 +21,31 @@ s.populations(1).name='L1';
 s.populations(1).size=7;
 s.populations(1).equations=eqns1;
 s.populations(1).mechanism_list={'iNa','iK'};
-s.populations(1).parameters={'Iapp',3,'gNa',120,'gK',36,'noise',10};
+s.populations(1).parameters={'Iapp',3,'gNa',120,'gK',36,'noise',50};
 
 s.populations(2).name='L2';
 s.populations(2).size=8;
 s.populations(2).equations=eqns1;
 s.populations(2).mechanism_list={'iNa','iK'};
-s.populations(2).parameters={'Iapp',4,'gNa',90,'gK',12,'noise',25};
+s.populations(2).parameters={'Iapp',4,'gNa',90,'gK',12,'noise',10};
 
 s.populations(3).name='L3';
 s.populations(3).size=5;
 s.populations(3).equations=eqns1;
 s.populations(3).mechanism_list={'iNa','iK'};
-s.populations(3).parameters={'Iapp',3,'gNa',70,'gK',24,'noise',25};
+s.populations(3).parameters={'Iapp',3,'gNa',70,'gK',24,'noise',10};
 
 s.populations(4).name='L4';
 s.populations(4).size=12;
 s.populations(4).equations=eqns2;
 s.populations(4).mechanism_list={'iNa','iK'};
-s.populations(4).parameters={'Iapp',9,'gNa',360,'gK',48,'noise',10};
+s.populations(4).parameters={'Iapp',9,'gNa',360,'gK',48,'noise',30};
 
 s.populations(5).name='L5';
 s.populations(5).size=6;
 s.populations(5).equations=eqns2;
 s.populations(5).mechanism_list={'iNa','iK'};
-s.populations(5).parameters={'Iapp',20,'gNa',360,'gK',36,'noise',40};
+s.populations(5).parameters={'Iapp',20,'gNa',360,'gK',36,'noise',60};
 
 s.connections(1).direction='L1->L2';
 s.connections(1).mechanism_list={'iGABAa'};
