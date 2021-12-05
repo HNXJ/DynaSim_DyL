c256e055e3583c27c78b0f8b6c0f6184dd06c921
Hamed Nejat <hamednejat7@gmail.com>
2021-12-05 19:53:57 +0330

    V2.0.1

--- main.m
+++ main.m
@@ -36,37 +36,37 @@ s.populations(1).name='L1';
 s.populations(1).size=7;
 s.populations(1).equations=eqns1;
 s.populations(1).mechanism_list={'iNa','iK'};
-s.populations(1).parameters={'Iapp',1,'gNa',40,'gK',36,'noise',5};
+s.populations(1).parameters={'Iapp',2,'gNa',40,'gK',36,'noise',5};
 
 s.populations(2).name='L2';
 s.populations(2).size=8;
 s.populations(2).equations=eqns1;
 s.populations(2).mechanism_list={'iNa','iK'};
-s.populations(2).parameters={'Iapp',1,'gNa',50,'gK',12,'noise',10};
+s.populations(2).parameters={'Iapp',2,'gNa',50,'gK',12,'noise',10};
 
 s.populations(3).name='L3';
 s.populations(3).size=5;
 s.populations(3).equations=eqns1;
 s.populations(3).mechanism_list={'iNa','iK'};
-s.populations(3).parameters={'Iapp',0.5,'gNa',70,'gK',24,'noise',10};
+s.populations(3).parameters={'Iapp',1.5,'gNa',70,'gK',24,'noise',10};
 
 s.populations(4).name='L4';
 s.populations(4).size=12;
 s.populations(4).equations=eqns2;
 s.populations(4).mechanism_list={'iNa','iK'};
-s.populations(4).parameters={'Iapp',2,'gNa',250,'gK',36,'noise',20};
+s.populations(4).parameters={'Iapp',4,'gNa',250,'gK',36,'noise',20};
 
 s.populations(5).name='L5';
 s.populations(5).size=6;
 s.populations(5).equations=eqns2;
 s.populations(5).mechanism_list={'iNa','iK'};
-s.populations(5).parameters={'Iapp',2,'gNa',250,'gK',36,'noise',20};
+s.populations(5).parameters={'Iapp',4,'gNa',250,'gK',36,'noise',20};
 
 s.populations(6).name='L6';
 s.populations(6).size=4;
 s.populations(6).equations=eqns2;
 s.populations(6).mechanism_list={'iNa','iK'};
-s.populations(6).parameters={'Iapp',2,'gNa',360,'gK',36,'noise',20};
+s.populations(6).parameters={'Iapp',4,'gNa',360,'gK',36,'noise',20};
 
 s.populations(7).name='Input1';
 s.populations(7).size=1;
@@ -121,9 +121,10 @@ s.connections(10).mechanism_list={'iAMPA'};
 s.connections(10).parameters={'tauD',3,'gAMPA',.5,'netcon', 'rand(N_pre,N_post)'}; 
 
 m = DynaModel(s);
+m.simulate(100, 0.01);
 a = m.connections;
 disp('done');
 
 % data=dsSimulate(s);
 % data = dsSimulate(s, 'solver', 'rk1', 'dt', .01, 'downsample_factor', 10, 'verbose_flag',1);
-% dsPlot(data); 
+dsPlot(m.data); 
