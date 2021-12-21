9dd994a6f3a6580eec767c61a356f583a488a183
Hamed Nejat <hamednejat7@gmail.com>
2021-12-21 04:47:36 +0330

    V2.0.5

--- main.m
+++ main.m
@@ -33,67 +33,67 @@ eqns_input2={
 
 s=[];
 
-s.populations(1).name='L1-2-3';
-s.populations(1).size=12;
+s.populations(1).name='L1I';
+s.populations(1).size=16;
 s.populations(1).equations=eqns_superficial;
 s.populations(1).mechanism_list={'iNa','iK'};
-s.populations(1).parameters={'Iapp',1,'gNa',120,'gK',36,'noise',10};
+s.populations(1).parameters={'Iapp', 1, 'gNa', 120, 'gK', 36, 'noise',10};
 
-s.populations(2).name='L2';
-s.populations(2).size=9;
-s.populations(2).equations=eqns1;
+s.populations(2).name='L2I';
+s.populations(2).size=8;
+s.populations(2).equations=eqns_deep;
 s.populations(2).mechanism_list={'iNa','iK'};
 s.populations(2).parameters={'Iapp',1,'gNa',120,'gK',36,'noise',10};
 
-s.populations(3).name='L3';
-s.populations(3).size=6;
-s.populations(3).equations=eqns1;
+s.populations(3).name='L3I';
+s.populations(3).size=12;
+s.populations(3).equations=eqns_deep;
 s.populations(3).mechanism_list={'iNa','iK'};
 s.populations(3).parameters={'Iapp',1,'gNa',120,'gK',36,'noise',10};
 
-s.populations(4).name='L4';
-s.populations(4).size=12;
-s.populations(4).equations=eqns2;
+s.populations(4).name='L1E';
+s.populations(4).size=16;
+s.populations(4).equations=eqns_superficial;
 s.populations(4).mechanism_list={'iNa','iK'};
-s.populations(4).parameters={'Iapp',1,'gNa',120,'gK',48,'noise',10};
+s.populations(4).parameters={'Iapp', 1, 'gNa', 120, 'gK', 36, 'noise',10};
 
-s.populations(5).name='L5';
-s.populations(5).size=6;
-s.populations(5).equations=eqns2;
+s.populations(5).name='L2E';
+s.populations(5).size=8;
+s.populations(5).equations=eqns_deep;
 s.populations(5).mechanism_list={'iNa','iK'};
-s.populations(5).parameters={'Iapp',1,'gNa',120,'gK',48,'noise',10};
+s.populations(5).parameters={'Iapp',1,'gNa',120,'gK',36,'noise',10};
 
-s.populations(6).name='L6';
-s.populations(6).size=6;
-s.populations(6).equations=eqns2;
+s.populations(6).name='L3E';
+s.populations(6).size=12;
+s.populations(6).equations=eqns_deep;
 s.populations(6).mechanism_list={'iNa','iK'};
-s.populations(6).parameters={'Iapp',1,'gNa',120,'gK',48,'noise',10};
+s.populations(6).parameters={'Iapp',1,'gNa',120,'gK',36,'noise',10};
 
 s.populations(7).name='Input1';
 s.populations(7).size=1;
-s.populations(7).equations=eqns3;
+s.populations(7).equations=eqns_input1;
 s.populations(7).mechanism_list={'iNa','iK'};
 s.populations(7).parameters={'Iapp',1,'gNa',120,'gK',36,'noise',1};
 
 s.populations(8).name='Input2';
 s.populations(8).size=1;
-s.populations(8).equations=eqns4;
+s.populations(8).equations=eqns_input2;
 s.populations(8).mechanism_list={'iNa','iK'};
 s.populations(8).parameters={'Iapp',1,'gNa',120,'gK',36,'noise',1};
 
-s.connections(1).direction='L1->L2';
+s.connections(1).direction='L1E->L1I';
 s.connections(1).mechanism_list={'iGABAa'};
 s.connections(1).parameters={'tauD',5,'gGABAa',.1,'netcon','rand(N_pre,N_post)'};
 
-s.connections(2).direction='L2->L1';
+s.connections(2).direction='L1I->L1E';
 s.connections(2).mechanism_list={'iAMPA'};
 s.connections(2).parameters={'tauD',5,'gAMPA',.1,'netcon','rand(N_pre,N_post)'};
 
-s.connections(3).direction='L5->L2';
+s.connections(3).direction='L1I->L2I';
 s.connections(3).mechanism_list={'iAMPA'};
 s.connections(3).parameters={'tauD',5,'gAMPA',.1,'netcon', 'rand(N_pre,N_post)'}; 
 
-s.connections(4).direction='L2->L5';
+s.connections(4).direction='L1E->L2I';
 s.connections(4).mechanism_list={'iGABA'};
 s.connections(4).parameters={'tauD',5,'gGABAa',.1,'netcon', 'rand(N_pre,N_post)'}; 
 
