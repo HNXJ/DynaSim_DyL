62c1fe839dbbfc5e56b44623a8bcc160f954b57e
Hamed Nejat <hamednejat7@gmail.com>
2022-01-31 07:23:45 +0330

    V8.1.9

--- testbench.m
+++ testbench.m
@@ -92,6 +92,7 @@ vary = [];
 % vary = {'E', 'onset_pfc_poisson', [100 300]};
 %     'I', 'onset_pfc_poisson', [100 300]};
 % vary = {'I->E', 'tauGABA', [2 4]};
+
 data=dsSimulate(s,'vary',vary,'tspan',tspan,simulator_options{:});
 
 % Plots results
